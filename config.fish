bca37edd1b1463287d3bb173f7f6716cf1ab6142
slick <rcglb627@gmail.com>
2018-02-22 13:46:30 -0600

    bconf backup

--- config.fish
+++ config.fish
@@ -3,5 +3,4 @@ set fish_greeting
 # set editor
 set -x EDITOR "nvim"
 # source functions files
-source ~/.config/fish/functions/aliases.fish
-source ~/.config/fish/functions/export.fish
+source ~/.config/fish/functions/*.fish
