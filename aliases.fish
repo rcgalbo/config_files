e1499e0594b91e292a19f0d5c193566702093c1e
slick <rcglb627@gmail.com>
2018-02-22 13:46:56 -0600

    bconf backup

--- aliases.fish
+++ aliases.fish
@@ -1,24 +1,14 @@
 # base aliases {{{
 alias vi="nvim"
-alias c=clear
 alias reload="source ~/.config/fish/conf.d"
 alias ll="ls -la"
-alias sum="paste -sd+ - | bc"
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias clean="git reset --hard HEAD; and git clean -df"
-alias attach="tmux attatch"
-alias detatch="tmux detatch"
 alias cvs="rm ~/.local/share/nvim/swap/*" 
-alias chrome="google-chrome"
 # }}}
 # py/conda aliases{{{
-alias py="python"
 alias actv="conda activate"
-# }}}
-# git aliases {{{
-alias gs="git status"
-alias gc="git clone"
 # }}}
 # {{{ tmux aliases
 alias tks="tmux kill-session"
@@ -32,10 +22,6 @@ alias tsw="tmux switch"
 alias ev="vi ~/.config/nvim/init.vim"
 alias eff="vi ~/.config/fish/config.fish"
 alias ef="vi ~/.config/fish/functions/"
-# }}}
-# quick source {{{
-alias sv="source  ~/.config/nvim/init.vim"
 alias sf="source ~/.config/fish/config.fish"
 # }}}
-
 # vim:foldmethod=marker:foldlevel=0
---
+++ bconf.fish
@@ -0,0 +1,14 @@
+function bconf
+  set backup_dir = ~/doc/config_files/
+  cp ~/.config/nvim/init.vim $backup_dir
+  cp ~/.tmux.conf $backup_dir
+  cp ~/.config/fish/config.fish $backup_dir  
+  cp ~/.config/fish/functions/*.fish $backup_dir 
+  cd ~/doc/config_files/
+  git add --all
+  git status
+  git commit -m "bconf backup"
+  git push -u origin master
+  cd
+end
+
